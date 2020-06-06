2828450e95b8c8e3bf28e41263c54f282272063f
JiHyeonMon <jhk8806@naver.com>
2020-06-07 00:53:42 +0900

    마이페이지 이동

--- Sopkerton/app/src/main/java/com/danmin/sopkerton/MainActivity.kt
+++ Sopkerton/app/src/main/java/com/danmin/sopkerton/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.danmin.sopkerton
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MypageActivity::class.java)
+            startActivity(intent)
+        }
 
         mainAdapter = MainAdapter(this)
         rv_home.run {
