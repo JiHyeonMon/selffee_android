02b3bc1d90e0d19eeae07efdb8acfacb5ffd804c
leejieun1121 <lje4648@naver.com>
2020-06-07 01:34:14 +0900

    jieun branch 생성

--- Sopkerton/app/src/main/java/com/danmin/sopkerton/mainRecycler/MainViewHolder.kt
+++ Sopkerton/app/src/main/java/com/danmin/sopkerton/mainRecycler/MainViewHolder.kt
@@ -8,13 +8,13 @@ import com.bumptech.glide.Glide
 import com.danmin.sopkerton.R
 
 class MainViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-    val username = itemView.findViewById<TextView>(R.id.username)
-    val img_profile = itemView.findViewById<ImageView>(R.id.img_profile)
-    val img_content = itemView.findViewById<ImageView>(R.id.img_content)
+//    val username = itemView.findViewById<TextView>(R.id.username)
+//    val img_profile = itemView.findViewById<ImageView>(R.id.img_profile)
+//    val img_content = itemView.findViewById<ImageView>(R.id.img_content)
 
     fun bind(mainData: MainData){
-        username.text = mainData.username
-        Glide.with(itemView).load(mainData.img_profile).into(img_profile)
-        Glide.with(itemView).load(mainData.img_content).into(img_content)
+//        username.text = mainData.username
+//        Glide.with(itemView).load(mainData.img_profile).into(img_profile)
+//        Glide.with(itemView).load(mainData.img_content).into(img_content)
     }
 }
