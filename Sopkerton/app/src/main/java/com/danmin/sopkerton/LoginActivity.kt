17c667bec8c42ec4fc5e04bbada03ccf74075579
leejieun1121 <lje4648@naver.com>
2020-06-07 05:02:19 +0900

    Loginactivity디자인

---
+++ Sopkerton/app/src/main/java/com/danmin/sopkerton/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.danmin.sopkerton
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
